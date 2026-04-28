742d4817a51aeebad989d57595f5b4e22358c15f
din3th-4 <sheharadineth14@gmail.com>
2026-04-28 19:01:49 +0200

    deepface_analyses_every20frames_outputs_just_ the emotion

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+frame_count = 0 
+
+last_emotion = "analysing..."
+
 while True:
 
     ret, frame = cap.read()
@@ -11,12 +15,24 @@ while True:
     if not ret:
         continue
 
-    frame1 = cv2.resize(frame, (0,0), fx=0.3, fy=0.3)
-    framemir = cv2.flip(frame1, 1) 
-    
-    
+    frame = cv2.resize(frame, (0,0), fx=0.3, fy=0.3)
+    frame = cv2.flip(frame, 1) 
 
-    cv2.imshow('webcam', framemir)  
+    if frame_count % 20 == 0:
+        try:
+            result = DeepFace.analyze(frame, actions= ['emotion'], enforce_detection= False) 
+            last_emotion = result[0]['dominant_emotion']
+        except Exception as e:
+            print(e)
+    
+    frame_count += 1 
+    
+    cv2.putText(frame, last_emotion, (20, 40),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+    
+    cv2.imshow('webcam', frame)  
+
+    
 
     if cv2.waitKey(1) == ord('q'):
         break 
