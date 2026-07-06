0f1068954bc73d60071948d73d2757bd6bfaff6a
din3th-4 <sheharadineth14@gmail.com>
2026-07-06 15:07:48 +0200

    zz

--- mediapipe_landmarks.py
+++ mediapipe_landmarks.py
@@ -5,9 +5,32 @@ correlate to the ratings assigned by humans and AI
 """
 
 import os 
-import sys 
-import mediapipe as mp
-import cv2
-import numpy as np
+import csv 
 
-from utilsss import get_img_list, get_dir_label, parse_filenames, get_already_rated, csv_fields 
+import mediapipe as mp 
+from mediapipe.tasks import python as mp_python
+from mediapipe.tasks.python import vision as mp_vision
+
+
+from utilsss import get_img_list, get_dir_label, parse_filenames, get_already_rated, valid_ext
+
+model_path = "face_landmarker.task"
+m_name = "mediapipe"
+csv_path = f"{m_name}.csv" 
+
+features = [
+    "browDownLeft", "browDownRight", "browInnerUp",
+    "browOuterUpLeft", "browOuterUpRight", "eyeSquintLeft", 
+    "eyeSquintRight", "eyeWideLeft", "eyeWideRight",
+    "jawOpen", "mouthSmileLeft", "mouthSmileRight",
+    "mouthFrownLeft", "mouthFrownRight", "mouthPucker",
+    "noseSneerLeft", "noseSneerRight", "cheekSquintLeft", 
+    "cheekSquintRight",
+] 
+
+csv_fields = (["img_id", "version", "ambiguous", "r_id", "directory"] 
+              + features
+            )
+
+
+
